c75d8fbb5095b95f7f91199af1bc0046aa0e4369
Igor Diakonov <aidos.tanatos@gmail.com>
2020-02-12 15:07:33 +0300

    Tests are fixed

--- pkg/template/template_test.go
+++ pkg/template/template_test.go
@@ -20,13 +20,31 @@ func TestRenderTemplate(t *testing.T) {
 		Return("secret1", nil).
 		Times(1)
 
+	template := "The secret is '{{ vault \"secret/my/test/secret\" \"field1\" }}'."
+
+	renderer := VaultTemplateRenderer{
+		vaultClient: mockVaultClient,
+	}
+
+	result, err := renderer.RenderTemplate(template)
+
+	assert.NilError(t, err)
+	assert.Equal(t, result, "The secret is 'secret1'.")
+}
+
+func TestRenderMapTemplate(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockVaultClient := api.NewMockVaultClient(mockCtrl)
+
 	mockVaultClient.
 		EXPECT().
 		QuerySecretMap("secret/my/test/secret").
 		Return(map[string]interface{}{"field1": "secret1"}, nil).
 		Times(1)
 
-	template := "The secret is '{{ vault \"secret/my/test/secret\" \"field1\" }}'."
+	template := "{{ $m := vaultMap \"secret/my/test/secret\" }}The secret is '{{ $m.field1 }}'."
 
 	renderer := VaultTemplateRenderer{
 		vaultClient: mockVaultClient,
@@ -50,13 +68,30 @@ func TestRenderTemplateQueryError(t *testing.T) {
 		Return("", errors.New("test error")).
 		Times(1)
 
+	template := "The secret is '{{ vault \"secret/my/test/secret\" \"field1\" }}'."
+
+	renderer := VaultTemplateRenderer{
+		vaultClient: mockVaultClient,
+	}
+
+	_, err := renderer.RenderTemplate(template)
+
+	assert.Assert(t, err != nil)
+}
+
+func TestRenderMapTemplateQueryError(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockVaultClient := api.NewMockVaultClient(mockCtrl)
+
 	mockVaultClient.
 		EXPECT().
 		QuerySecretMap("secret/my/test/secret").
 		Return(nil, errors.New("test error")).
 		Times(1)
 
-	template := "The secret is '{{ vault \"secret/my/test/secret\" \"field1\" }}'."
+	template := "The secret is '{{ vaultMap \"secret/my/test/secret\" }}'."
 
 	renderer := VaultTemplateRenderer{
 		vaultClient: mockVaultClient,
